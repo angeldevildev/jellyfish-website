d064f12f2a60f1e31d6d03aab8a6b9d052f5cc73
mineshopdev <mineshop47@gmail.com>
2024-10-05 18:06:02 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
     title: "JellyFishMC",
     description: "JellyFishMC è un server MineCraft italiano a più modalità attualmente in costruzione, ma puoi giocare la survival proprio ORA!! Unisciti al server, ti aspettiamo!",
     url: "https://jellyfish-website.vercel.app",
+    siteName: "JellyFishMC",
     images: [
       {
         url: "https://jellyfish-website.vercel.app/api/og",
